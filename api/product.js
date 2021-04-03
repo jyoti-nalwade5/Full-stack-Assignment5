e6f07ea00f7f6a672983f9bc83c1dab607dfa619
jyoti5 <jyoti.nalwade5@gmail.com>
2021-04-02 17:35:18 -0700

    Backend Modules

---
+++ api/product.js
@@ -0,0 +1,20 @@
+/* eslint linebreak-style: ["error", "windows"] */
+const { getDb, getNextSequence } = require('./db.js');
+
+async function list() {
+    const db = getDb();
+    const products = await db.collection('products').find({}).toArray();
+    return products;
+}
+
+async function add(_, { product }) {
+    const db = getDb();
+    const newProduct = Object.assign({}, product);
+    newProduct.id = await getNextSequence('products');
+    const result = await db.collection('products').insertOne(newProduct);
+    const savedProduct = await db.collection('products')
+      .findOne({ _id: result.insertedId });
+    return savedProduct;
+}
+
+module.exports = { list, add };
