499904ea9f861918ca3946437374ea0c50fec5bb
jyoti5 <jyoti.nalwade5@gmail.com>
2021-04-11 16:52:08 -0700

    final

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -26,14 +26,14 @@ if (enableHMR && (process.env.NODE_ENV !== 'production')) {
 app.use(express.static('public'));
 
 const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT
-  || 'http://localhost:3001/graphql';
+  || 'http://localhost:3000/graphql';
 const env = { UI_API_ENDPOINT };
 
 app.get('/env.js', (req, res) => {
   res.send(`window.ENV = ${JSON.stringify(env)}`);
 });
 
-const port = process.env.UI_SERVER_PORT || 8001;
+const port = process.env.UI_SERVER_PORT || 8000;
 
 app.listen(port, () => {
   console.log(`UI started on port ${port}`);
